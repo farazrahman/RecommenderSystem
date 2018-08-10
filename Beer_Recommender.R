9d82d24a8699106f48947e3f0e4c66bdc6da0f49
farazrahman <faraz.rahman41@gmail.com>
2018-08-11 00:04:52 +0530

    Using minimum reviews as 200

--- Beer_Recommender.R
+++ Beer_Recommender.R
@@ -32,6 +32,11 @@ ggplot(missing_data, aes(x = reorder(variables, percent_missing), y = percent_mi
 
 beer <- beer[complete.cases(beer),]
 
+#Removing duplicated beer reviews
+
+beer <- beer[!duplicated(beer[c(1,2)]),]
+str(beer) #Now there are 474462 unique beer id and profile combinations
+
 #II- Exploratory Data Analysis
 
 #What are the ranges of ratings
@@ -52,18 +57,116 @@ beer%>% group_by(review_overall) %>% summarise(total = length(review_overall)) %
 
 #only 0.69% and 0.80% respondents have provided ratings of 1 and 1.5 respectively and around 36.73% of beers have 4 rating.
 
-#How many reviews each beer has
+#How many reviews each beer has.
 
 beer %>% group_by(beer_beerid) %>% summarise(total = length(beer_beerid)) %>% #filter(total <= 10)%>%
   mutate(percent = (total/sum(total)*100))%>%
- ggplot(aes(total))+ geom_histogram(bins = 1000)
+ ggplot(aes(total))+ geom_histogram(bins = 30)
 
-beer %>% group_by(beer_beerid) %>% summarise(total = length(beer_beerid)) %>% #filter(total <= 10)%>%
-  mutate(average = mean(total))
- 
+beer %>% group_by(beer_beerid) %>% summarise(total = length(beer_beerid)) %>% filter(total <= 10)%>%
+  mutate(average = mean(total))%>%
+  ggplot(aes(x = "", y = total)) + geom_boxplot()
+
+#Let's take 12 as minimum number of reviews a beer category has.
+
+beer <- beer %>% group_by(beer_beerid) %>%  filter(length(beer_beerid)>= 12)
+str(beer)
+#Converting to realRatingMatrix
+
+beer_df <- as.data.frame(beer)
+r <- as(beer_df, "realRatingMatrix")
+str(r)
+
+# get some informtaion
+dimnames(r)
+rowCounts(r)
+colCounts(r)
+rowMeans(r)
 
 
+##Data Exploration
+
+#1-How similar are the first ten users are with each other
+
+similar_users <- similarity(r[1:10, ],
+                            method = "cosine",
+                            which = "users")
+
+#Similarity matrix
+as.matrix(similar_users)
+
+#Visualise similarity matrix
+image(as.matrix(similar_users), main = "User similarity")
 
 
+#2-How similar are the first 10 beers items are with each other
+
+similar_items <- similarity(r[,1:10 ],
+                            method = "cosine",
+                            which = "items")
+as.matrix(similar_items)
+
+image(as.matrix(similar_items), main = "Item similarity")
+
+
+#3- What are the unique values of ratings?
+
+ggplot(beer, aes(factor(review_overall), fill = factor(review_overall)))+geom_bar()+theme_bw()
+
+#4- Visualise the rating values and notice:
+
+#The average beer ratings
+
+qplot(getRatings(r), binwidth = 1, 
+      main = "Histogram of ratings", xlab = "Rating")
+
+summary(getRatings(r)) # The average beer rating is 3.858
+
+qplot(getRatings(normalize(r, method = "Z-score")),
+      main = "Histogram of normalized ratings", xlab = "Rating") 
+
+summary(getRatings(normalize(r, method = "Z-score"))) 
+
+
+qplot(rowCounts(r), binwidth = 10, 
+      main = "Beers Rated on average", 
+      xlab = "# of users", 
+      ylab = "# of movies rated")
+#Most users rate less number of beers.
+#Very few users have rated more beers
+
+#The average user ratings
+
+mean_userRating <- beer %>% group_by(review_profilename) %>% summarise(meanRating = mean(review_overall))
+  ggplot(mean_userRating, aes(x = meanRating)) + geom_histogram()
+
+summary(mean_userRating$meanRating) #mean user rating is 3.924
+
+#The average number of ratings given to the beers
+
+meanRating_beers <- beer %>% group_by(beer_beerid) %>% summarise(meanRating = length(beer_beerid))
+
+summary(meanRating_beers$meanRating) #63 ratings
+
+#The average number of ratings given by the users
+
+meanRating_users <- beer %>% group_by(review_profilename) %>% summarise(meanRating = length(review_profilename))
+
+summary(meanRating_users$meanRating) #18 ratings
+
+#5. Recommendation Models
+
+#Divide your data into training and testing datasets
+
+#Experiment with 'split' and 'cross-validation' evaluation schemes
+
+#List of models available
+recommender_models <- recommenderRegistry$get_entries(dataType = "realRatingMatrix")
+names(recommender_models)# 9 types of models
+
+#Divide data into test 
+scheme <- evaluationScheme(r, method = "split", train = .9,
+                           k = 1, given = 2, goodRating = 4)
+scheme
 
 
